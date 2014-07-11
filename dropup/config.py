4134b1a69fe15bd4e94d9690794b58b186042628
zhangbo <zhangbo3@yy.com>
2014-07-10 19:19:35 -0700

    remove upyun:user,pwd, add comments

--- dropup/config.py
+++ dropup/config.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+'''Config module with app key and secrets'''
+
 class DROPBOXCONFIG:
     APP_KEY = 'ctgzx31jpej1elr'
     APP_SEC = 'vrcl6n7chiztgbj'
 
 class UPYUNCONFIG:
     BUCKETNAME = 'dropboxxx'
-    USERNAME = 'zhangbolinux'
-    PASSWORD = 'zb891115'
 
